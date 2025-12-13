719555f78b3f3a59e5a73364d1b6fd980c51b3e9
mayrlex <mayrlex.h@gmail.com>
2025-12-13 12:25:18 +0500

    feat: add a simple receipt of product information and display it on the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-function App() {
+import { fetchProducts } from "./api";
+
+const products = await fetchProducts();
+
+const App = () => {
 
   return (
     <>
+      <div>
+        {products.map(product => (
+          <div key={product.id}>
+            <h2>{product.title}</h2>
+            <p>{product.description}</p>
+            <p>${product.price}</p>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
